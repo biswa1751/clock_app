92ecd1b7db2830827efbeb895c87e092299546f0
Biswajit Behera <suraj53028@gmail.com>
2025-03-08 15:59:45 +0530

    issue fix

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,44 +6,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Color(0xFFC2DE14), Color(0xFFD1B964)],
+      body: DefaultTabController(
+        length: 3,
+        child: Stack(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Color(0xFFC2DE14), Color(0xFFD1B964)],
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 60.0),
-          
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TabBar(
-                      tabs: [
-                        Tab(text: "Time"),
-                        Tab(text: "Stopwatch"),
-                        Tab(text: "Timer"),
-                      ],
-                    ),
-                    Container(
-                      height: 250,
-                      width: 250,
-                      margin: const EdgeInsets.only(top: 50.0),
-                      color: Colors.red,
-                    ),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 60.0),
+
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TabBar(
+                        tabs: [
+                          Tab(text: "Time"),
+                          Tab(text: "Stopwatch"),
+                          Tab(text: "Timer"),
+                        ],
+                      ),
+                      Container(
+                        height: 250,
+                        width: 250,
+                        margin: const EdgeInsets.only(top: 50.0),
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
